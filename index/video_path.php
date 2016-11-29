63ce0a0998d62cb64389c1a5ce6e6f7c01a74846
koez <ahmadfakhrul20@yahoo.com>
2016-11-29 12:26:35 +0800

    Update video_path.php
    Give it a go

--- index/video_path.php
+++ index/video_path.php
@@ -47,6 +47,111 @@ if($display)
 
 	<?php } } ?>
 
+<?php 
+//the block to store the video
+$name= $_FILES['file']['name'];
+
+$tmp_name= $_FILES['file']['tmp_name'];
+
+$submitbutton= $_POST['submit'];
+
+$position= strpos($name, "."); 
+
+$fileextension= substr($name, $position + 1);
+
+$fileextension= strtolower($fileextension);
+
+$description= $_POST['description_entered'];
+
+if (isset($name)) {
+
+$path= 'Uploads/videos/';
+
+if (!empty($name)){
+if (($fileextension !== "mp4") && ($fileextension !== "ogg") && ($fileextension !== "webm"))
+{
+echo "The file extension must be .mp4, .ogg, or .webm in order to be uploaded";
+}
+
+
+else if (($fileextension == "mp4") || ($fileextension == "ogg") || ($fileextension == "webm"))
+{
+if (move_uploaded_file($tmp_name, $path.$name)) {
+echo 'Uploaded!';
+}
+}
+}
+}
+?>
+
+
+<?php//for insertion
+
+//Block 1
+$user = "user"; 
+$password = "password"; 
+$host = "host"; 
+$dbase = "database"; 
+$table = "table"; 
+
+
+
+//Block 3
+$connection= mysql_connect ($host, $user, $password);
+if (!$connection)
+{
+die ('Could not connect:' . mysql_error());
+}
+mysql_select_db($dbase, $connection);
+
+
+
+//Block 4
+if(!empty($description)){
+mysql_query("INSERT INTO $table (description, filename, fileextension)
+VALUES ('$description', '$name', '$fileextension')");
+}
+
+
+//Block 5
+mysql_close($connection);
+
+?>
+
+<?php// display the video
+$user = "user"; 
+$password = "password"; 
+$host = "host"; 
+$dbase = "database"; 
+$table = "table"; 
+ 
+// Connection to DBase 
+mysql_connect($host,$user,$password); 
+@mysql_select_db($dbase) or die("Unable to select database");
+
+$result= mysql_query( "SELECT description, filename, fileextension FROM $table ORDER BY ID" ) 
+or die("SELECT Error: ".mysql_error()); 
+
+print "<table border=1>\n"; 
+while ($row = mysql_fetch_array($result)){ 
+$videos_field= $row['filename'];
+$video_show= "Uploads/videos/$videos_field";
+$descriptionvalue= $row['description'];
+$fileextensionvalue= $row['fileextension'];
+print "<tr>\n"; 
+print "\t<td>\n"; 
+echo "<font face=arial size=4/>$descriptionvalue</font>";
+print "</td>\n";
+print "\t<td>\n"; 
+echo "<div align=center><video width='320' controls><source src='$video_show' type='video/$fileextensionvalue'>Your browser does
+not support the video tag.</video></div>";
+print "</td>\n";
+print "</tr>\n"; 
+} 
+print "</table>\n"; 
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
